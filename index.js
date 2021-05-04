670a77436ac1bd11985c9e0512cc98ac010acb39
Benjamin Wang <benjylxwang64@gmail.com>
2021-05-04 21:13:01 +0100

    Added pizzaz

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const clickCount = 420;
+const clickCount = 42;
 
 const vals = ["4", "four", "Four", "四", "0100"]
-let i = Math.random() % vals.length;
+let i = Math.floor(Math.random() * clickCount * 100) % vals.length;
 let count = 0;
 
 function changeNumber() {
+    document.getElementById("party").style.display = "none";
+    i = (i + 1) % vals.length;
     document.getElementById("number").innerText = vals[i];
 
-    i = (i + 1) % vals.length;
-
     count++;
+    console.log(count);
     if (count == clickCount) {
-        document.getElementById("number").innerText = Math.random();
+        document.getElementById("number").innerText = Math.floor(Math.random()* clickCount * 100);
+        document.getElementById("party").style.display = "initial";
         count = 0;
     }
 }
